f78bf43e55d798b1008628f3bd98fad9de50b4c3
Delyer <jiayaoit@126.com>
2016-10-13 11:17:53 -0700

    Initial Commit

---
+++ ThreadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThreadDemo
+//
+//  Created by Dely on 16/10/13.
+//  Copyright © 2016年 Dely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThreadDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThreadDemo
+//
+//  Created by Dely on 16/10/13.
+//  Copyright © 2016年 Dely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
